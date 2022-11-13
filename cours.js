e3db732d026009d04f460fc4239addc656509f66
cbrickley-matrice <cbrickley@matrice.io>
2022-11-13 22:31:41 +0100

    first

---
+++ cours.js
@@ -0,0 +1,52 @@
+
+//HyperText Markup Language
+// un langage de balisage (avec des balises) conçu pour représenter des pages web / que comprend les navigateurs. Donc ce sont des instructions pour les navigateurs. 
+//cf schéma page 6 : le navigateur va interprêter le fichier HTML, tout comme le CSS pour réaliser l'affichage d'un site sur votre écran.
+//https://developer.mozilla.org/fr/docs/Learn/CSS/First_steps/How_CSS_works
+
+ 
+
+//HTML 
+// décomposition d'une balise 
+// https://developer.mozilla.org/fr/docs/Glossary/HTML#concept_et_syntaxe
+
+
+//listes 
+// des éléménts (des balises)
+
+// des attributs 
+// https://developer.mozilla.org/fr/docs/Web/HTML/Attributes
+
+
+// installation d'Emmet (raccourci avec le !)
+// montrer Doctype 
+//https://developer.mozilla.org/fr/docs/Glossary/Doctype
+// html : indiquer au navigateur qu'il lit un document html, c'est le premier élément de l'arbre du DOM qu'il recréé
+// head : 	L'élément HTML <head> fournit des informations générales (métadonnées) sur le document, incluant son titre et des liens ou des définitions vers des scripts et feuilles de style.
+// meta : 
+// link : L'élément HTML <link> définit la relation entre le document courant et une ressource externe. Cet élément peut être utilisé pour définir un lien vers une feuille de style, vers les icônes utilisées en barre de titre ou comme icône d'application sur les appareils mobiles.
+
+
+//qu'est ce que vous connaissez comme principale balise ?
+// cf. slide 13 
+// faire les exercices d'Rbean sans la sentinelle (à partir de red_square)
+// faire tous les exercices de la journée 2 (sauf bordered table et spaced table)
+// on ne fait pas les exercices de la journée 3
+
+// faire la démo inline-block et flex
+// il y a des éléments qui prennent toute la largeur par défaut : ce sont des éléments blocks
+//les div, les sections, les p, les h etc.
+// il y a des éléments inline par défaut : les span, strong, a
+// inline-block : c'est pour mettre des éléments block en inline
+//https://openclassrooms.com/fr/courses/1603881-creez-votre-site-web-avec-html5-et-css3/3298561-faites-votre-mise-en-page-avec-flexbox
+
+
+//CSS 
+//la classe est prioritaire
+// l'id est plus fort de la classe
+// plus j'ai de détail plus c'est fort div .class 
+//l'id aura toujours la priorité
+// quand on doit surcoucher (sans toucher à des id par exemple), important peut sauver la mise 
+
+
+// utilisation de tailwind et saas dans les projets pour plus tard 
